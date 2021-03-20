d17a605156a691546ceb6a1bcf988644d36c3302
Justin Heyes-Jones <justinhj@gmail.com>
2021-03-20 13:09:38 -0700

    delete unused

--- src/main/scala/org/justinhj/Magicratelimiter.scala
+++ src/main/scala/org/justinhj/Magicratelimiter.scala
@@ -6,26 +6,25 @@ import zio.clock._
 import sttp.client3.httpclient.zio._
 import hn._
 
-object Magicratelimiter extends App {
+object GetTopItem extends App {
 
-  val exampleApp = {
+  val app = {
     for (
-      _ <- putStrLn("Fetching item");
-      item <- Client.getItem(26498527);
-      _ <- putStrLn(s"Item: $item");
-      _ <- putStrLn("Fetching user");
-      user <- Client.getUser("justinhj");
-      _ <- putStrLn(s"User: $user");
-      _ <- putStrLn("Fetching top stories");
-      topStories <- Client.getTopStories();
-      _ <- putStrLn(s"Top stories: $topStories");
-      maxItem <- Client.getMaxItem();
-      _ <- putStrLn(s"Top stories: $maxItem")
+      _ <- putStrLn("Fetching stories");
+      stories <- Client.getTopStories();
+      _ <- putStrLn(s"Received ${stories.itemIDs.size} stories");
+      _ <- if(stories.itemIDs.size == 0)
+            ZIO.fail("No stories :(")
+           else
+            for (
+              item <- Client.getItem(stories.itemIDs.head);
+              _ <- putStr(s"Top story ${item.title} ${item.url}")
+            ) yield ()
      ) yield ()
   }
 
   val zLayers = Clock.live ++ Console.live ++ HttpClientZioBackend.layer()
 
-  def run(args: List[String]) = exampleApp.provideLayer(zLayers).
+  def run(args: List[String]) = app.provideLayer(zLayers).
     fold(_ =>ExitCode(1), _ => ExitCode(0))
 }
