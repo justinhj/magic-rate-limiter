7e00b658183074e44bfc0d635b049b98287df693
Justin Heyes-Jones <justinhj@gmail.com>
2021-03-25 14:34:42 -0700

    Couple more tests

--- src/test/scala/org/justinhj/hn/ClientSpec.scala
+++ src/test/scala/org/justinhj/hn/ClientSpec.scala
@@ -15,7 +15,6 @@ import zio.magic._
 import zio.test.Assertion._
 import zio.test._
 
-
 object ClientSpec extends DefaultRunnableSpec {
 
   val configLayer = ZLayer.succeed(RateLimiterConfig(50.milliseconds))
@@ -27,15 +26,35 @@ object ClientSpec extends DefaultRunnableSpec {
       RateLimiter.live,
       HttpClientZioBackend.stubLayer)
 
+  val sampleUserJSON = """{
+    "about" : "This is a test",
+    "created" : 1173923446,
+    "id" : "jl",
+    "karma" : 4240,
+    "submitted" : [ 25172559, 25172553]
+  }""".stripMargin
+
   val maxItemStub = for {
     _ <- whenRequestMatches(_.uri.toString.endsWith("maxitem.json")).thenRespond("26575791")
+    _ <- whenRequestMatches(_.uri.toString.endsWith("topstories.json")).thenRespond("[ 26583791, 26582729, 26580926, 26580477 ]")
+    _ <- whenRequestMatches(_.uri.toString.endsWith("user/jl.json")).thenRespond(sampleUserJSON)
   } yield ()
 
   def spec = suite("ClientSpec")(
-      testM("getMaxItem retrieves and parses correct value") {
+      testM("getMaxItem retrieves and parses correctly") {
         (for {
           maxItem <- maxItemStub *> Client.getMaxItem()
         } yield assert(maxItem)(equalTo(Data.HNSingleItemID(26575791)))).provideLayer(testLayer)
+      },
+      testM("getTopStories retrieves and parses correctly") {
+        (for {
+          maxItem <- maxItemStub *> Client.getTopStories()
+        } yield assert(maxItem)(equalTo(Data.HNItemIDList(List(26583791, 26582729, 26580926, 26580477))))).provideLayer(testLayer)
+      },
+      testM("getTopeStories retrieves and parses correctly") {
+        (for {
+          maxItem <- maxItemStub *> Client.getUser("jl")
+        } yield assert(maxItem)(equalTo(Data.HNUser("jl", 1173923446, 4240, "This is a test", List(25172559,25172553))))).provideLayer(testLayer)
       }
     )
 
